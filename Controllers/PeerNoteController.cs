41d9a12ab5f08879c994e2834e6d09ffa4c71a97
Garrett Beaverson <gb538515@ohio.edu>
2020-03-24 22:20:23 -0400

    Added a blank recognition page for code and removed some files that were non existent and stopping the page from running

---
+++ Controllers/PeerNoteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team_16_Centric_Project.Controllers
+{
+    public class PeerNoteController : Controller
+    {
+        // GET: PeerNote
+        public ActionResult Index()
+        {
+            ViewBag.ItemList = "Recognize a Peer for Their Centric Values";
+            return View();
+        }
+    }
+}
