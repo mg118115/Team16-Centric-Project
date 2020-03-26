88a22b1b5840509a08a21c12d9d4872c878bb51e
Garrett Beaverson <gb538515@ohio.edu>
2020-03-26 14:25:33 -0400

    Created a Recognition Page that is more dynamic and incorporates the email code.

---
+++ Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HtmlHelperDemo.Models
+{
+    public class UserModel
+    {
+        public string Email { get; set; }
+
+        public string Value { get; set; }
+
+        public string Message { get; set; }
+    }
+}
