5c5b61e64dd14200c76486d3cfc2f2554a2da6b7
Matthew Gilligan <mg118115@ohio.edu>
2020-03-31 11:36:43 -0400

    created user controller and model and DAL page

---
+++ Content/DAL/MIS4200Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Team_16_Centric_Project.Models;
+using System.Data.Entity;
+
+namespace Team_16_Centric_Project.Content.DAL 
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+
+        {
+
+            // this method is a 'constructor' and is called when a new context is created
+
+            // the base attribute says which connection string to use
+
+        }
+
+        // Include each object here. The value inside <> is the name of the class,
+
+        // the value outside should generally be the plural of the class name
+
+        // and is the name used to reference the entity in code
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Team_16_Centric_Project.Models
+{
+    public class User
+    {
+        public int userId { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [Required]
+        [Display(Name = "Business Unit")]
+        public string businessUnit { get; set; }
+
+        [Required]
+        [Display(Name = "Hire Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> hireDate { get; set; }
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+    }
+}
