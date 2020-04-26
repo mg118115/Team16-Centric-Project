ecfac0ed35c82614b49f399f157c9e1e306b9363
Garrett Beaverson <gb538515@ohio.edu>
2020-04-26 16:44:15 -0400

    Added button for database and removed useless links

--- Models/User.cs
+++ Models/User.cs
@@ -16,10 +16,7 @@ namespace Team_16_Centric_Project.Models
 
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+
        
         [Required]
         [Display(Name = "First Name")]
